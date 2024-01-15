0cd4075694eae9f1e730b62b55f5f5b00302413a
Leegipyo <dlrlvy7909@naver.com>
2024-01-15 17:47:18 +0900

    이기표, Push Test

--- team/src/team/Main.java
+++ team/src/team/Main.java
@@ -1,7 +1,10 @@
 package team;
 
 public class Main {
+
 	public static void main(String[] args) {
-		System.out.println("¾ö¼±¿µ");
+		System.out.println("Hello");
+		System.out.println("Test");
 	}
+
 }
