4cab4fb16f5913c43b6bc69d0b764090bc783cc0
ilung2 <doqjjong4@naver.com>
2024-01-15 17:59:05 +0900

    작성자 정일웅

--- team/src/team/Main.java
+++ team/src/team/Main.java
@@ -1,12 +1,14 @@
 package team;
-
+/**
+ * 
+ * @author 정일웅
+ *
+ */
 public class Main {
-
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Test");
 		System.out.println("Hi");
 		System.out.println("네");
 	}
-
 }
