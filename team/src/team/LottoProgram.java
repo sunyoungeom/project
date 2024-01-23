77568501d58c91b117f9155980907a69de114f46
Leegipyo <dlrlvy7909@naver.com>
2024-01-23 09:34:24 +0900

    이기표 커밋

--- team/src/team/LottoProgram.java
+++ team/src/team/LottoProgram.java
@@ -52,6 +52,7 @@ public class LottoProgram extends JFrame {
 		}
 
 		setTitle("메인 창");
+
 		JPanel main = new JPanel();
 		JLabel backgroundImage = new JLabel(new ImageIcon("images/lottoMainimage.png"));
 		SpringLayout sl_main = new SpringLayout();
@@ -75,11 +76,11 @@ public class LottoProgram extends JFrame {
 		sl_main.putConstraint(SpringLayout.WEST, btnResult, 0, SpringLayout.WEST, btnBuy);
 		sl_main.putConstraint(SpringLayout.EAST, btnResult, 308, SpringLayout.WEST, btnBuy);
 		sl_main.putConstraint(SpringLayout.NORTH, btnBefore, 0, SpringLayout.SOUTH, btnResult);
-		sl_main.putConstraint(SpringLayout.SOUTH, btnBefore, 85, SpringLayout.SOUTH, btnResult);
+		sl_main.putConstraint(SpringLayout.SOUTH, btnBefore, 83, SpringLayout.SOUTH, btnResult);
 		sl_main.putConstraint(SpringLayout.WEST, btnBefore, 0, SpringLayout.WEST, btnResult);
 		sl_main.putConstraint(SpringLayout.EAST, btnBefore, 0, SpringLayout.EAST, btnResult);
 		sl_main.putConstraint(SpringLayout.NORTH, btnHelp, 0, SpringLayout.SOUTH, btnBefore);
-		sl_main.putConstraint(SpringLayout.SOUTH, btnHelp, 78, SpringLayout.SOUTH, btnBefore);
+		sl_main.putConstraint(SpringLayout.SOUTH, btnHelp, 80, SpringLayout.SOUTH, btnBefore);
 		sl_main.putConstraint(SpringLayout.WEST, btnHelp, 0, SpringLayout.WEST, btnBefore);
 		sl_main.putConstraint(SpringLayout.EAST, btnHelp, 0, SpringLayout.EAST, btnBefore);
 
@@ -124,9 +125,9 @@ public class LottoProgram extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (resultBuy.get(0).size() != 0) {
-				setVisible(false);
-				HelpFrame helpFrame = new HelpFrame(LottoProgram.this);
-				helpFrame.setVisible(true);
+					setVisible(false);
+					HelpFrame helpFrame = new HelpFrame(LottoProgram.this);
+					helpFrame.setVisible(true);
 				}
 			}
 		});
